b39fa95d950db16b75c95f51532ddd0f0ae6002c
kylosilly <gangggevil@gmail.com>
2025-02-25 15:17:16 +0100

    Update astolfo ware loader.lua

--- astolfo ware loader.lua
+++ astolfo ware loader.lua
@@ -2,18 +2,16 @@ local rape = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main
 
 local Lib = loadstring(game:HttpGet(rape .. 'Library.lua'))()
 
-local instance = game.PlaceId
+local Instance = game.PlaceId
 local Market = game:GetService("MarketplaceService")
-local info = Market:GetProductInfo(instance)
+local Info = Market:GetProductInfo(Instance)
 
-if instance == 78360449985300 then
+if Instance == 78360449985300 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/kylosilly/astolfoware/refs/heads/main/scripts/Disch.lua"))()
 else
-    Lib:Notify('Game not supported: ' .. info.Name)
-    Lib:Notify('Copied Discord to your clipboard')
-    setclipboard('https://discord.gg/frQv5QScXS')
+    Lib:Notify('Game not supported: ' .. Info.Name)
 end
 
-if instance == game.PlaceId then
-    Lib:Notify('Supported! loading: ' .. info.Name)
+if Instance == game.PlaceId then
+    Lib:Notify('Supported! loading: ' .. Info.Name)
 end
