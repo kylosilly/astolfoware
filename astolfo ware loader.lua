1d29105eb8dd6b0d8bb83ad388295f22585b08a0
kylosilly <gangggevil@gmail.com>
2025-02-25 14:28:21 +0100

    Create astolfo ware loader.lua

---
+++ astolfo ware loader.lua
@@ -0,0 +1,16 @@
+local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+local Lib = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+
+local instance = game.PlaceId
+local market = game:GetService("MarketplaceService")
+local instanceinfo = market:GetProductInfo(instance)
+
+if instance == 78360449985300 then --// Disch
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kylosilly/astolfoware/refs/heads/main/Disch.lua"))()
+elseif instance == 4483381587 then --// a literal baseplate.
+    print("test")
+else
+    Lib:Notify('Game not found. If you want it to be supported, send this to the owner: ' .. instanceinfo.Name)
+    Lib:Notify('Copied Discord to your clipboard')
+    setclipboard('https://discord.gg/frQv5QScXS')
+end
