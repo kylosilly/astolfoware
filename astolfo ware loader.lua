d8d8b71453959fa146ada95685bc5e3735fcf966
kylosilly <gangggevil@gmail.com>
2025-02-27 19:44:59 +0100

    Update astolfo ware loader.lua

--- astolfo ware loader.lua
+++ astolfo ware loader.lua
@@ -1,10 +1,20 @@
-local rape = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+--// Feel free to use this loader source 🐾 <3
+local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
 
-local Lib = loadstring(game:HttpGet(rape .. 'Library.lua'))()
+local Lib = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 
+--// Services
 local Chart = game.PlaceId
 local Market = game:GetService("MarketplaceService")
 local Info = Market:GetProductInfo(Chart)
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+
+--// Tables
+local Friends = {1660649535, 873084588, 1740785260, 608503596}
+local Developer = {1412075257, 12967961}
+local BigRat = 7060863999
+local Testers = 573649346
 
 setclipboard('https://discord.gg/frQv5QScXS')
 Lib:Notify('Copied discord link to your clipboard')
@@ -15,3 +25,20 @@ if Chart == 78360449985300 then
 else
     Lib:Notify('Game not supported: ' .. Info.Name)
 end
+
+--// Thanks ! nfpw for fixing this <3
+function RoleCheck(Player)
+    if Player.Character and Player.Character:FindFirstChild("Humanoid") then local Humanoid = Player.Character:FindFirstChild("Humanoid")
+        if table.find(Friends, Player.UserId) then
+            Humanoid.DisplayName = " [Friend] "..Player.DisplayName
+        elseif table.find(Developer, Player.UserId) then
+            Humanoid.DisplayName = " [Developer] "..Player.DisplayName
+        elseif Player.UserId == BigRat then
+            Humanoid.DisplayName = "🐀 [Big Rat] "..Player.DisplayName
+        elseif Player.UserId == Testers then
+            Humanoid.DisplayName = "🧪 [Tester] "..Player.DisplayName
+        end
+    end
+end
+
+for _, p in pairs(Players:GetPlayers()) do RoleCheck(p) end; Players.PlayerAdded:Connect(function(Player) RoleCheck(Player); Player.CharacterAdded:Connect(function() RoleCheck(Player) end) end)
