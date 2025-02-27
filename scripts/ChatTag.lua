a339a3afd6776bc2e7d261f9d77a38209c08ef75
kylosilly <gangggevil@gmail.com>
2025-02-27 23:03:38 +0100

    Update ChatTag.lua

--- scripts/ChatTag.lua
+++ scripts/ChatTag.lua
@@ -7,7 +7,7 @@ local BigRat = 7060863999
 local Testers = 573649346
 
 function RoleCheck(Player)
-    if Player.Character and Player.Character:FindFirstChild("Humanoid") ~= nil then 
+    if Player.Character and Player.Character:FindFirstChild("Humanoid") ~= nil then
         local Humanoid = Player.Character.Humanoid
         if table.find(Friends, Player.UserId) then
             Humanoid.DisplayName = " [Friend] "..Player.DisplayName
@@ -21,4 +21,4 @@ function RoleCheck(Player)
     end
 end
 
-for _, p in pairs(Players:GetPlayers()) do if p.Character then RoleCheck(p) end if not CharacterAddedConnections[p] then CharacterAddedConnections[p] = p.CharacterAdded:Connect(function(char) RoleCheck(Players:GetPlayerFromCharacter(char)) end) end end Players.PlayerAdded:Connect(function(Player) CharacterAddedConnections[Player] = Player.CharacterAdded:Connect(function(char) RoleCheck(Players:GetPlayerFromCharacter(char)) end) end) Players.PlayerRemoving:Connect(function(Player) if CharacterAddedConnections[Player] then CharacterAddedConnections[Player]:Disconnect() CharacterAddedConnections[Player] = nil end end)
+for _, p in pairs(Players:GetPlayers()) do if p.Character then RoleCheck(p) end if not CharacterAddedConnections[p] then CharacterAddedConnections[p] = p.CharacterAdded:Connect(function(char) task.wait(0.05) RoleCheck(p) end) end end Players.PlayerAdded:Connect(function(Player) CharacterAddedConnections[Player] = Player.CharacterAdded:Connect(function(char) task.wait(0.05) RoleCheck(Player) end) end) Players.PlayerRemoving:Connect(function(Player) if CharacterAddedConnections[Player] then CharacterAddedConnections[Player]:Disconnect() CharacterAddedConnections[Player] = nil end end)
