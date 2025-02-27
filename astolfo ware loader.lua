a7f5ea02aa51d46be852df6ed437ea85a72dc20b
kylosilly <gangggevil@gmail.com>
2025-02-27 17:06:52 +0100

    Update astolfo ware loader.lua

--- astolfo ware loader.lua
+++ astolfo ware loader.lua
@@ -6,6 +6,9 @@ local Chart = game.PlaceId
 local Market = game:GetService("MarketplaceService")
 local Info = Market:GetProductInfo(Chart)
 
+setclipboard('bleh')
+Lib:Notify('Copied discord link to your clipboard')
+
 if Chart == 78360449985300 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/kylosilly/astolfoware/refs/heads/main/scripts/Disch.lua"))()
     Lib:Notify('Supported! loading: ' .. Info.Name)
