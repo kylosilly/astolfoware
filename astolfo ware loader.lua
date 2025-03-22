37c7722e9bb029ea8b22ec1425e6a37438d045ba
kylosilly <gangggevil@gmail.com>
2025-03-22 02:55:10 +0100

    Update astolfo ware loader.lua

--- astolfo ware loader.lua
+++ astolfo ware loader.lua
@@ -1,32 +1,32 @@
---// Feel free to use this loader source 🐾 <3
-local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kylosilly/astolfoware/refs/heads/main/scripts/ChatTag.lua"))()
+-- Feel free to use this loader source 🐾 <3
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kylosilly/astolfoware/refs/heads/main/scripts/ChatTag.lua'))()
 
-local Lib = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local scripts = 'https://raw.githubusercontent.com/kylosilly/astolfoware/main/scripts/'
 
---// Services
-local Chart = game.PlaceId
-local Market = game:GetService("MarketplaceService")
-local Info = Market:GetProductInfo(Chart)
-local LocalPlayer = game:GetService("Players").LocalPlayer
+local lplr = game:GetService("Players").LocalPlayer
+local market = game:GetService("MarketplaceService")
+local info = market:GetProductInfo(game.PlaceId)
+local place_id = game.PlaceId
 
 if identifyexecutor() then
     local executor = identifyexecutor()
     if executor == "Solara" or executor == "Xeno" or executor == "Nezur" then
-        LocalPlayer:Kick("Unsupported executor: " .. executor .. " Please use atlantis or a diffirent executor copied invite to your clipboard")
+        lplr:Kick("Unsupported executor: " .. executor .. " Please use atlantis or a diffirent executor copied invite to your clipboard")
         setclipboard("https://discord.gg/KRkqCJjFG4")
     end
 end
 
-if Info.Name == "[🐍 LUNAR YEAR] Horrific Housing" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/kylosilly/astolfoware/refs/heads/main/scripts/Horrific%20Housing.lua"))()
-    Lib:Notify('Supported! loading: ' .. Info.Name)
-elseif Info.Name == "SPECTER Classic [AI]" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/kylosilly/astolfoware/refs/heads/main/scripts/Specter.lua"))()
-    Lib:Notify('Supported! loading: ' .. Info.Name)
-elseif Info.Name == "SPECTER" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/kylosilly/astolfoware/refs/heads/main/scripts/SpecterLobby.lua"))()
-    Lib:Notify('Supported! loading: ' .. Info.Name)
+if place_id == 263761432 then
+    loadstring(game:HttpGet(scripts.."Horrific%20Housing.lua"))()
+    lib:Notify('Supported game loading: ' .. info.Name)
+elseif place_id == 8267733039 then
+    loadstring(game:HttpGet(scripts.."SpecterLobby.lua"))()
+    lib:Notify('Supported game loading: ' .. info.Name)
+elseif info.Name == "SPECTER Classic [AI]" then
+    loadstring(game:HttpGet(scripts.."Specter.lua"))()
+    lib:Notify('Supported game loading: ' .. info.Name)
 else
-    Lib:Notify('Game not supported: ' .. Info.Name)
+    lib:Notify('Game not supported: ' .. info.Name .. ' if you wanna support this game join the discord copied to your clipboard')
+    setclipboard("https://discord.gg/U5yjDvsxHR")
 end
