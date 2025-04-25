03b1928440e07ecee9e3a11d5dbe68d6b285361a
kylosilly <gangggevil@gmail.com>
2025-04-25 12:19:39 +0200

    Update skid duels.lua

--- scripts/skid duels.lua
+++ scripts/skid duels.lua
@@ -11,59 +11,52 @@ local local_player = game:GetService('Players').LocalPlayer
 local tween_service = game:GetService('TweenService')
 local run_service = game:GetService('RunService')
 local workspace = game:GetService('Workspace')
-
-local enabled = true
+local camera = workspace.CurrentCamera
 
 task.wait(5)
 
 if local_player.TeamColor == BrickColor.new("Bright red") then
     library:Notify("Starting Auto Farm!")
     while true do
-        if enabled then
-            local velocity_connection; velocity_connection = run_service.Heartbeat:Connect(function()
-                if local_player.Character.HumanoidRootPart then
-                    local_player.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
-                    local_player.Character.HumanoidRootPart.AssemblyAngularVelocity = Vector3.zero
-                end
-            end)
-            if local_player.PlayerGui.Hotbar.MainFrame.GameEndFrame.Visible then
-                library:Notify("Game Ended, Teleporting To Next Match...")
-                local enabled = false
-                velocity_connection:Disconnect()
-                replicated_storage:WaitForChild("Modules"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("MatchService"):WaitForChild("RF"):WaitForChild("EnterQueue"):InvokeServer("Solo")
-                return
+        local connections; connections = run_service.Heartbeat:Connect(function()
+            if local_player.Character.HumanoidRootPart then
+                local_player.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
+                local_player.Character.HumanoidRootPart.AssemblyAngularVelocity = Vector3.zero
             end
-            local blue_finish = workspace.Map.BlueBase.Goal
-            local distance = (blue_finish.Position - local_player.Character.HumanoidRootPart.Position).magnitude
-            local tween = tween_service:Create(local_player.Character.HumanoidRootPart, TweenInfo.new(distance / 25, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = CFrame.new(blue_finish.Position)})
-            tween:Play()
-            tween.Completed:Wait()         
-            task.wait(5)
+        end)
+        if local_player.PlayerGui.Hotbar.MainFrame.GameEndFrame.Visible then
+            library:Notify("Game Ended, Teleporting To Next Match...")
+            velocity_connection:Disconnect()
+            replicated_storage:WaitForChild("Modules"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("MatchService"):WaitForChild("RF"):WaitForChild("EnterQueue"):InvokeServer("Solo")
+            return
         end
+        local blue_finish = workspace.Map.BlueBase.Goal
+        local distance = (blue_finish.Position - local_player.Character.HumanoidRootPart.Position).magnitude
+        local tween = tween_service:Create(local_player.Character.HumanoidRootPart, TweenInfo.new(distance / 25, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = CFrame.new(blue_finish.Position)})
+        tween:Play()
+        tween.Completed:Wait()         
+        task.wait(5)
     end
 elseif local_player.TeamColor == BrickColor.new("Bright blue") then
     library:Notify("Starting Auto Farm!")
     while true do
-        if enabled then
-            local velocity_connection; velocity_connection = run_service.Heartbeat:Connect(function()
-                if local_player.Character.HumanoidRootPart then
-                    local_player.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
-                    local_player.Character.HumanoidRootPart.AssemblyAngularVelocity = Vector3.zero
-                end
-            end)
-            if local_player.PlayerGui.Hotbar.MainFrame.GameEndFrame.Visible then
-                library:Notify("Game Ended, Teleporting To Next Match...")
-                local enabled = false
-                velocity_connection:Disconnect()
-                replicated_storage:WaitForChild("Modules"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("MatchService"):WaitForChild("RF"):WaitForChild("EnterQueue"):InvokeServer("Solo")
-                return
+        local connections; connections = run_service.Heartbeat:Connect(function()
+            if local_player.Character.HumanoidRootPart then
+                local_player.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
+                local_player.Character.HumanoidRootPart.AssemblyAngularVelocity = Vector3.zero
             end
-            local red_finish = workspace.Map.RedBase.Goal
-            local distance = (red_finish.Position - local_player.Character.HumanoidRootPart.Position).magnitude
-            local tween = tween_service:Create(local_player.Character.HumanoidRootPart, TweenInfo.new(distance / 25, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = CFrame.new(red_finish.Position)})
-            tween:Play()
-            tween.Completed:Wait()     
-            task.wait(5)
+        end)
+        if local_player.PlayerGui.Hotbar.MainFrame.GameEndFrame.Visible then
+            library:Notify("Game Ended, Teleporting To Next Match...")
+            velocity_connection:Disconnect()
+            replicated_storage:WaitForChild("Modules"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("MatchService"):WaitForChild("RF"):WaitForChild("EnterQueue"):InvokeServer("Solo")
+            return
         end
+        local red_finish = workspace.Map.RedBase.Goal
+        local distance = (red_finish.Position - local_player.Character.HumanoidRootPart.Position).magnitude
+        local tween = tween_service:Create(local_player.Character.HumanoidRootPart, TweenInfo.new(distance / 25, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), {CFrame = CFrame.new(red_finish.Position)})
+        tween:Play()
+        tween.Completed:Wait()     
+        task.wait(5)
     end
 end
